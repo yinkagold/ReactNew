61708024a14ceab16e22f7dbcf56ab1d38962ad0
Yinka Gold <speedroc@yahoo.com>
2025-03-21 12:54:31 +0200

    counter-app changes

---
+++ src/components/counter.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = { 
+        count:0
+     };
+     
+     styles = {
+        fontsize: 20,
+        fontWeight: 'bold'
+     }
+    render() { 
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return (
+            <div> 
+                <span className={classes}>{this.state.count}</span>
+                <button className='btn btn-secondary btn-sm'>Increment</button>
+            </div>
+       );
+    }
+   
+}
+ 
+export default Counter;
