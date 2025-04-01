c4cd9f3a56e6613f59b4bd6f8845158c53e834de
Yinka Gold <speedroc@yahoo.com>
2025-04-01 12:54:00 +0300

    tags, badges and state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,23 +2,41 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = { 
-        count:0
+        count:0,
+        tags: ["tag1", "tag2" ]
      };
+
+    // constructor(){
+     //   super();
+      //  this.handleIncrement = this.handleIncrement.bind(this);
+     //}
      
-     styles = {
-        fontsize: 20,
-        fontWeight: 'bold'
+     handleIncrement = () => {
+        console.log("increment Clicked", this)
      }
+
     render() { 
-        let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
         return (
             <div> 
-                <span className={classes}>{this.state.count}</span>
+                <span className={this.getBadgeClasses()}>{this.state.count}</span>
                 <button className='btn btn-secondary btn-sm'>Increment</button>
+                <ul>
+                    {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+                </ul>
             </div>
        );
     }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount() {
+        const { value } = this.state.count;
+        return value === 0 ? "Zero" : value;
+      }
    
 }
  
